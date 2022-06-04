208c7f736fe979003a57294c90836a71de14da00
Ahmad Wafi Fathurrahman <104130485+ahmadwafii@users.noreply.github.com>
2022-06-05 04:20:54 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,231 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Digital World Company</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome-free-5.15.4-web/css/all.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="a"><strong>PT. Digital World</a></strong>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" style="color: rgb(0, 68, 255);" aria-current="page" href="menu.php">Home</a>
+                    <a class="nav-link" style="color: rgb(0, 68, 255);" href="promosi.php">Promosi yang Diinginkan</a>
+                    <a class="nav-link" style="color: rgb(0, 68, 255);" href="kontak kami.php">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron">
+        <h1 class="display-4 text-center">PT. Digital World</h1>
+        <p class="lead text-center">Analysis, Hasil, dan Terapkan</p>
+    </div>
+    <hr>
+    <div class="mt-3 text-center">
+        <h2 span class="line"><i class="ico-user5"></i></span>Tentang Kami</h2>
+    </div>
+    <div class="bg-light">
+        <div class="container text-justify p-4">
+            PT. Global Digital, Tbk merupakan perusahaan yang bergerak di bidang peralatan Internet of Things(IoT) dan
+            Computer Vision.
+
+            Perusahaan kami menyediakan berbagai kebutuhan digital device, jasa analytics data, dan Development
+            Algorithm Intelligent.
+
+            Perusahaan yang diberi nama Global Digital ini diharapkan dapat bergerak secara universal, menjangkau ke
+            seluruh kalangan, dan menjadi andalan di dunia Tech dan Computing Intelligent.
+        </div>
+    </div>
+    <hr>
+
+    <div class="mt-3 text-center">
+        <h2 span class="line"><i class="ico-user5"></i></span>Struktur Perusahaan</h2>
+    </div>
+    <center>
+        <img width="700" alt="Person Name" loading="lazy" class="lozad" src="image/Struktur Perusahaan.jpeg">
+
+    </center>
+    <hr>
+
+    <div class="mt-3 text-center">
+        <h2 span class="line"><i class="ico-user5"></i></span>Anggota Perusahaan</h2>
+    </div>
+
+    <div class="foto_slide">
+        <div class="col-md-6 text-center">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Kinanthi Putri A</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">Administrator</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Nayya Ramadhani</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">Digital Marketing</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 text-center">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Ahmad Wafi Fathurrahman</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">Lead Programmer</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Regina Fortunata</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">AI Engineer</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 text-center">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Alisa Jihan</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">UI/UX Designer</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Alyssa Amorita</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">Finance Manager</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 text-center">
+            <div class="team_block2 clearfix">
+                <a class="member_img" href="image/IMG_20220605_002044.jpg" data-rel="magnific-popup">
+                    <img loading='lazy' alt="Samsul Hadi, S.Kom., M.I.M" src="image/IMG_20220605_002044.jpg" width="200" height="200" style="border-radius: 50%;">
+                </a>
+                <div class="team_detail">
+                    <a><span class="person_name">Iqbal Ramadhan Anniswa</span></a>
+                    <span class="person_jop">-</span>
+                    <span class="person_desc">CEO</span>
+                    <div class="social_media clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+
+    <h2 class="mt-3 text-center">Kontak Kami</h2>
+
+    <div class="bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 p-4">
+                    <div class="form-group">
+                        <label><strong>Nama</strong></label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><strong>Company Name</strong></label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><strong>Address</strong></label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><strong>Email Address</strong></label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><strong>Phone Number</strong></label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label><strong>Message</strong></label>
+                        <textarea text="text" class="form-control" rows="5"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="section_footer">
+        <div class="footer_section_2">
+            <div class="container text-center p-4 text_center">
+                <div class="col-md-4 text-white">
+                    <p class="font-weight-bold">PT. Digital World</p>
+                    Jl. Kampung Durian Runtuh No. 29, Kuala Lumpur, Malaysia<br>
+                    Tel. : 083856096281<br>Email:digitalworld@gmail.com<br>
+                </div>
+            </div>
+        </div>
+        <div class="social_icon">
+            <ul>
+                <li><a href="#"><img src="image/fb-icon.png"></a></li>
+                <li><a href="https://twitter.com/ruangbolaa_"><img src="image/twitter-icon.png"></a></li>
+                <li><a href="#"><img src="image/linkdin-icon.png"></a></li>
+                <li><a href="https://instagram.com/ahmadwafii._"><img src="image/instagram-icon.png"></a></li>
+            </ul>
+        </div>
+    </footer>
+
+    </div>
+    <div class="copyright_section">
+        <div class="container">
+            <p class="copyright_text">Copyright &copy; 2022 All Rights Reserved by Ahmad Wafi Fathurrahman</p>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
